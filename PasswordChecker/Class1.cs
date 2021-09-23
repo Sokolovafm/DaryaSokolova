c8b35f5358dbda2f460135da765ada8834e1d6b2
Sokolovafm <90152615+Sokolovafm@users.noreply.github.com>
2021-09-23 11:55:03 +0300

    Update Class1.cs

--- PasswordChecker/Class1.cs
+++ PasswordChecker/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,7 +24,10 @@ namespace password
             return true;
 
 
-        }
+            
+             static void Main() 
+             Console.WriteLine(validatePassword("ftferr6F#"));
+       }
 
     }
 }
