2b53a4f5d9ba07dbe3eb2cc0abef554743e01bf9
Sokolovafm <90152615+Sokolovafm@users.noreply.github.com>
2021-09-23 13:08:16 +0300

    Add files via upload

---
+++ exception/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <exception>
+
+int main() {
+	try {
+		std::cout << "Enter first number ";
+		double num1;
+		std::cin >> num1;
+
+		std::cout << "Enter second number ";
+		double num2;
+		std::cin >> num2;
+
+		if (num2 == 0) {
+			std::cout << "You can't divide by zero";
+		}
+		else {
+			double res = num1 / num2;
+			std::cout << res;
+		}
+	}
+	catch (const std::exception& err) {
+		std::cout << "Invalid input";
+	}
+
+
+	return 0;
+}
