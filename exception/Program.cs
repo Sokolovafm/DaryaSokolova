b3f5d68c6c0d322d4da3c064a9d3d3f458ef7790
Sokolovafm <90152615+Sokolovafm@users.noreply.github.com>
2021-09-23 13:14:40 +0300

    Update Program.cs

--- exception/Program.cs
+++ exception/Program.cs
@@ -1,35 +1,33 @@
-﻿
-    using System;
+using System;
 
-    namespace Division
+namespace Division
+{
+    class Program
     {
-        class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            double a, b;
+            try
             {
-                double a, b;
-                try
-                {
-                    Console.WriteLine("Enter first number: ");
-                    a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter first number: ");
+                a = Convert.ToDouble(Console.ReadLine());
 
-                    Console.WriteLine("Enter second number: ");
-                    b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter second number: ");
+                b = Convert.ToDouble(Console.ReadLine());
 
-                    if (b == 0)
-                        Console.WriteLine("you cannot divide by zero ");
-                    else
-                        Console.WriteLine("Result:" + " " + a / b);
+                if (b == 0)
+                    Console.WriteLine("you cannot divide by zero ");
+                else
+                    Console.WriteLine("Result:" + " " + a / b);
 
-                }
-                catch
-                {
-                    Console.WriteLine("Invalid input");
-                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input");
+            }
                 Console.WriteLine("Size of integer in bytes:" + " " + sizeof(int));
                 Console.WriteLine("Size of float in bytes:" + " " + sizeof(float));
             }
         }
 
-    }
-
+}
